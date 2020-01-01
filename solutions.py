687c6119cfeeba9c66664bbb385171c9bbcd6134
Roy Cheng <cqyycx@163.com>
2020-01-01 13:00:48 +0800

    Create solutions.py

---
+++ solutions.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  1 12:57:04 2020
+
+Leet Code Practice
+
+@author: RoyCheng
+"""
+
+# %% 
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+
+
+
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
+#%%
+# No.      
+# Difficulty:
+# Classification:
+# Comments: 
